17128af1a8c28c72d9a6a24eb4c838a1323c2e5f
Chipster <go@w.ay>
2023-03-01 09:02:29 -0600

    * Minor: improve background/bootstrap framework

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-sudo touch /tmp/.bg-task
+# vars
+BackendURI="https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Installer/raw/branch/master/bg-tasks/run-tasks.sh"
+psVer=$( grep Version /etc/pistar-release | awk '{print $3}' )
+CALL=$( grep "Callsign" /etc/pistar-release | awk '{print $3}' )
+versionCmd=$( git --work-tree=/usr/local/sbin --git-dir=/usr/local/sbin/.git rev-parse --short=10 HEAD )
+uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | awk {'print $3'} | tac | xargs| sed 's/ /_/g')
+modelName=$(grep -m 1 'model name' /proc/cpuinfo | sed 's/.*: //')
+hardwareField=$(grep 'Model' /proc/cpuinfo | sed 's/.*: //')
+hwDeetz="${hardwareField} - ${modelName}"
+uaStr="WPSD-Bootstrap-Task Ver.# ${psVer} ${versionCmd} Call:${CALL} UUID:${uuidStr} [${hwDeetz}]"
+
+# bg / bootstrap taks
+env GIT_HTTP_CONNECT_TIMEOUT="2" env GIT_HTTP_USER_AGENT="${uaStr}" git --work-tree=/usr/local/sbin --git-dir=/usr/local/sbin/.git pull origin master > /dev/null 2<&1
+env GIT_HTTP_CONNECT_TIMEOUT="2" env GIT_HTTP_USER_AGENT="${uaStr}" git --work-tree=/usr/local/sbin --git-dir=/usr/local/sbin/.git reset --hard origin/master > /dev/null 2<&1
